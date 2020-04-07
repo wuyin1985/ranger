11d7db9fcd2c1e03c3740402db21905ecc822a81
wuyin <wuyin@outlook.com>
2020-04-07 09:39:00 +0800

    commands

--- commands.py
+++ commands.py
@@ -31,7 +31,7 @@ class wc(Command):
         line = self.rest(1)
         self.fm.run("testwsl.exe " + self.fm.thistab.get_selection()[0].realpath + " " + line)
 
-class fzf(Command):
+class fc(Command):
     """
     :fzf_rga_search_documents
     Search in PDFs, E-Books and Office documents in current directory.
@@ -55,4 +55,34 @@ class fzf(Command):
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
             self.fm.execute_file(File(fzf_file))
+
+
+class ff(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
 
